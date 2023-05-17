3ebf8e0229aac340a2af7c05612609b7769ec317
Kirti Singh <kirtisingh1497@gmail.com>
2023-05-17 20:57:40 +0530

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Mockman from "mockman-js";
-
+import {Routes,Route} from "react-router-dom" 
 function App() {
   return (
     <div className="App">
-      <Mockman />
+
+      <Routes>
+        <Route path="/" element={<Mockman/>}/>
+      </Routes>
     </div>
   );
 }
