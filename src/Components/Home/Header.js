e2563193231f10fdfa3c21eb3d796ac15a1ef1c5
Kirti Singh <kirtisingh1497@gmail.com>
2023-05-22 03:18:54 +0530

    style: working on style of nav bat

--- src/Components/Home/Header.js
+++ src/Components/Home/Header.js
@@ -1,16 +1,17 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { LoginAuthContext } from "../../Context/LoginAuthContext"
-import { useContext } from "react";
+import "./headerStyle.css"
 export const Header = () => {
   const {isEncodedToken} = useContext(LoginAuthContext);
   console.log("in header",isEncodedToken);
-    return<>
-    <nav>
+    return<div className="header-container">
+    <nav className="nav-container">
         <Link to="/">Home</Link>
         {isEncodedToken ?<Link to="/profile">Profile</Link> : <Link to="/login">Login</Link>}
         <Link to="/wishlist">WishList</Link>
         <Link to="/cart">Cart</Link>
         {/* <Link to="/login">Login</Link> */}
       </nav>
-    </>
+    </div>
 }
