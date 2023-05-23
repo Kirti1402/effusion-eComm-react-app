3b2889e5874a6ccb6b9c4d3a688625671050505f
Kirti Singh <kirtisingh1497@gmail.com>
2023-05-24 01:09:56 +0530

    style: making profile component to center

--- src/Components/Profile/ProfilePage.js
+++ src/Components/Profile/ProfilePage.js
@@ -1,41 +1,47 @@
-import {useContext, useState} from "react";
+import { useContext, useState } from "react";
 import { LoginAuthContext } from "../../Context/LoginAuthContext";
 import { useNavigate } from "react-router-dom";
-import "./Profile.css"
+import "./Profile.css";
+import { Address } from "./Address";
 
 export const ProfilePage = () => {
   const navigate = useNavigate();
-  const { setIsLoggedIn, setIsEncodedToken} =useContext(LoginAuthContext);
+  const { setIsLoggedIn, setIsEncodedToken } = useContext(LoginAuthContext);
   const firstName = localStorage.getItem("firstName");
   const lastName = localStorage.getItem("lastName");
   const email = localStorage.getItem("email");
 
-  const [selecttab,setSelectTab] = useState({profile:true,address:false})
+  const [selecttab, setSelectTab] = useState({ profile: true, address: false });
 
-  const logOutHandler = () =>{
-    
-    console.log("clicked")
+  const logOutHandler = () => {
+    console.log("clicked");
     localStorage.removeItem("Encodedtoken");
     localStorage.removeItem("LoginItem");
     setIsLoggedIn(false);
-    setIsEncodedToken("")
-    navigate("/")
-  }
+    setIsEncodedToken("");
+    navigate("/");
+  };
 
   return (
-    <div>
+    <div className="profile-page-container">
+      <div className="btn-container">
+      <button>Profile Information</button>
+      <button>Address Information</button>
+      </div>
       <div>
-        <button>Profile Information</button>
-        {selecttab.profile &&  
-      <div>
-        <p>ProfilePage Page</p>
-        <p>Name : {firstName}{lastName}</p>
-        <p>Email :{email}</p>
-        <button onClick={()=>logOutHandler()}>Log Out</button>
-      </div>}
-        <button>Address Information</button>
-        </div>
-     
+      {selecttab.profile && (
+        <section>
+          <p>ProfilePage Page</p>
+          <p>
+            Name : {firstName}
+            {lastName}
+          </p>
+          <p>Email :{email}</p>
+          <button onClick={() => logOutHandler()}>Log Out</button>
+        </section>
+      )}
+      {selecttab.address && <Address />}
+      </div>
     </div>
   );
 };
