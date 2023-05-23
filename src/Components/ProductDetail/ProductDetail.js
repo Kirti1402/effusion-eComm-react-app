7a86cf7220e6ae1253cc04530558f3e1be43f5ae
Kirti Singh <kirtisingh1497@gmail.com>
2023-05-24 00:48:26 +0530

    feat: working on Cart fetch api

--- src/Components/ProductDetail/ProductDetail.js
+++ src/Components/ProductDetail/ProductDetail.js
@@ -1,39 +1,36 @@
-import {useContext, useEffect,useState} from 'react'
+import { useContext, useEffect, useState } from "react";
 import { ProductListingcontext } from "../../Context/ProductListContext";
 
-
-const ProductDetail =() => {
-  const {
-    storeProductId
-
-  } = useContext(ProductListingcontext);
-  const [storeProductDetail,setProductDetail] = useState({});
-
+const ProductDetail = () => {
+  const { storeProductId } = useContext(ProductListingcontext);
+  //localstate
+  const [storeProductDetail, setProductDetail] = useState({});
+  //method to get productdetail from api
   const getProductDetail = async () => {
-    console.log("productId",storeProductId)
-    const response = await fetch(`/api/products/${storeProductId}`,{
+    console.log("productId", storeProductId);
+    const response = await fetch(`/api/products/${storeProductId}`, {
       method: "GET",
     });
     const productIndivialData = await response.json();
     setProductDetail(productIndivialData.product);
-  }
+  };
+
   useEffect(() => {
-    getProductDetail()
-  },[])
+    getProductDetail();
+  }, []);
 
-
-  console.log("store",storeProductDetail)
   return (
-    <div>ProductDetail
-      {storeProductDetail && <div>
-        <p>{storeProductDetail.title}</p>
-        <p>{storeProductDetail.size}</p>
-        <p>{storeProductDetail.rating}</p>
-        </div>}
-    
+    <div>
+      ProductDetail
+      {storeProductDetail && (
+        <div>
+          <p>{storeProductDetail.title}</p>
+          <p>{storeProductDetail.size}</p>
+          <p>{storeProductDetail.rating}</p>
+        </div>
+      )}
     </div>
-    
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
