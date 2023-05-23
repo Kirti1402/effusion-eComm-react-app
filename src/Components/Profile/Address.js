3d36936b6caac02050ba4700bfc0c5daf4518649
Kirti Singh <kirtisingh1497@gmail.com>
2023-05-24 03:23:36 +0530

    feat: adding address

--- src/Components/Profile/Address.js
+++ src/Components/Profile/Address.js
@@ -11,37 +11,66 @@ export const Address = () => {
       state: "Punjab",
     },
   ]);
+  const [addObject,setAddObject] = useState({
+    name: "Henry Potter",
+    phone: "9920397890",
+    address: "Bellandur, Banglore",
+    city: "Paris",
+    pincode: 234567,
+    state: "Punjab",
+  })
+  let add = JSON.stringify(address);
+  localStorage.setItem("DefaultAddress", add);
+
+  const  updateForm = (e) =>{
+    let name = e.target.name;
+    console.log(name)
+    let value = e.target.value;
+    console.log("name:value",name,value)
+    setAddObject({
+        ...addObject,
+        [name]:value,
+    })
+}
+console.log(addObject)
+
 
   const [isAddBtn,setIsAddBtn] = useState(false);
+
+  const onClickAddAddress = (event) =>{
+    event.preventDefault();
+    setAddress([...address,addObject])
+    setIsAddBtn(!isAddBtn)
+  }
   return (<>
    
   { !isAddBtn &&  <button  className="add-button" type="submit" onClick={()=>setIsAddBtn(!isAddBtn)}>Add Address</button> }
     { isAddBtn &&
     <div className="form-container">
-      <form>
+      <form onSubmit={onClickAddAddress}>
         <div className="input-container">
           <label className="form-label">Name:</label>
-          <input className="input-field" type="text" />
+          <input name='name' className="input-field" type="text" onChange={updateForm}/>
         </div>
 
         <div className="input-container">
           <label className="form-label">Phone:</label>
-          <input className="input-field" type="tel" />
+          <input name='phone' className="input-field" type="tel" onChange={updateForm} />
         </div>
 
         <div className="input-container">
         <label className="form-label">City:</label>
-        <input className="input-field" type="text" />
+        <input  className="input-field" name='city' type="text" onChange={updateForm}/>
         </div>
 
         <div className="input-container">
         <label className="form-label">Pincode:</label>
-        <input className="input-field" type="text" />
+        <input className="input-field" name='pincode' type="text" onChange={updateForm}/>
         </div>
 
         <div className="input-container">
         <label className="form-label">State:</label>
-        <input className="input-field" type="text" />
+        <input className="input-field" type="text" name='state' onChange={updateForm} />
         </div>
 
         
@@ -65,6 +94,8 @@ export const Address = () => {
         <p><span className="address-title">City:</span><span className="address-value">{city}</span></p>
         <p><span className="address-title">Pincode:</span><span className="address-value">{pincode}</span></p>
         <p><span className="address-title">State:</span><span className="address-value">{state}</span></p>
+        <button className="add-edit-btn">Edit</button>
+        <button className="add-del-btn">Delete</button>
 
     </div>)}
     </>
