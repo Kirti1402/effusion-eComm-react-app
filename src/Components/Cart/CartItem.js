b574e5eb5fcaaf760a9535dde667dea43983e285
Kirti Singh <kirtisingh1497@gmail.com>
2023-06-04 17:11:28 +0530

    fix: style for card component

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,121 +1,115 @@
-import { useContext, useEffect ,useState} from "react";
-import {ProductListingcontext} from "../../Context/ProductListContext"
+import { useContext, useEffect, useState } from "react";
+import { ProductListingcontext } from "../../Context/ProductListContext";
 import { cartContext } from "../../Context/CartContext";
-import {  toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import { wishListContext } from "../../Context/wishListContext";
 import { RemoveFromWishList } from "../WishList/RemoveWishList";
 import { addWishList } from "../WishList/AddWishlist";
-
-
+import "./cart.css"
 
 export const CartItems = () => {
-  const [type,setType] = useState("increment");
-  const {cartList, setCartList} =useContext(ProductListingcontext);
-  const {addedToCartList,setAddedToCartList} = useContext(cartContext);
-  const {wishlist, setWishlist,wishlistItem,
-    setWishlistItem,} = useContext(wishListContext);
+  const [type, setType] = useState("increment");
+  const { cartList, setCartList } = useContext(ProductListingcontext);
+  const { addedToCartList, setAddedToCartList } = useContext(cartContext);
+  const { wishlist, setWishlist, wishlistItem, setWishlistItem } =
+    useContext(wishListContext);
   const token = localStorage.getItem("Encodedtoken");
 
   const getCartItem = async () => {
-    const response  = await fetch("/api/user/cart",{
+    const response = await fetch("/api/user/cart", {
       method: "GET",
       headers: {
         authorization: `"${token}"`, // Assuming 'token' holds the actual token value
       },
     });
     const cartProduct = await response.json();
-    setCartList(cartProduct.cart)
+    setCartList(cartProduct.cart);
+  };
 
-  }
+  console.log("CartList", cartList);
 
-  console.log("CartList",cartList)
+  useEffect(() => {
+    getCartItem();
+  }, [type]);
 
-  useEffect(()=>{
-      getCartItem();
-  },[type])
-
-  const onClickRemoveFromCart =(id,title)=>{
-    const updatedList = addedToCartList.filter(item => item !== id)
-    setAddedToCartList([...updatedList])
+  const onClickRemoveFromCart = (id, title) => {
+    const updatedList = addedToCartList.filter((item) => item !== id);
+    setAddedToCartList([...updatedList]);
     toast.warn(`${title} removed from cart!`, {
-      position: toast.POSITION.TOP_RIGHT
+      position: toast.POSITION.TOP_RIGHT,
     });
-    RemoveFromCart(id)
-  }
+    RemoveFromCart(id);
+  };
 
-  const RemoveFromCart = async (id) =>{
-
+  const RemoveFromCart = async (id) => {
     const response = await fetch(`/api/user/cart/${id}`, {
-        method: 'DELETE',
-        headers: {
-            authorization: token
-          },
-           })
-        const cartProduct = await response.json();
-        setCartList(cartProduct.cart)
-}
-
-const onclickHandleQty = (id,type) =>{
-
-  
-  console.log(id,type)
-
-    incrementTheProduct(id,type)
-
-  
-
-}
-const incrementTheProduct = async (id,type) =>{
-  const response = await fetch(`/api/user/cart/${id}`, {
-      method: 'Post',
+      method: "DELETE",
       headers: {
-          authorization: token
-        },
-        body : JSON.stringify({action:{type:type}})  
-         })
-         console.log("increment response",await response.json())
-}
-
-
-const totalPrice = cartList.length>0 && cartList.reduce((acc,curr) =>{
-  acc = acc + (curr.price * curr.qty)
-  return acc;
-},0)
-
-const handleToggleWishlist = (productItem) => {
-  let wishListProduct = { product:productItem}
-  if (wishlist.includes(productItem._id)) {
-    const updatedWishlist = wishlist.filter((item) => item !== productItem._id);
-    setWishlist(updatedWishlist);
-    RemoveFromWishList(productItem._id)
-    toast.warn(`${productItem.title} removed from wishlist!`, {
-      position: toast.POSITION.TOP_RIGHT
+        authorization: token,
+      },
     });
-  } else {
-    addWishList(wishListProduct)
-    setWishlist([...wishlist, productItem._id]);
-    toast.success(`${productItem.title} added to wishlist!`, {
-      position: toast.POSITION.TOP_RIGHT
-    });
-  }
-};
+    const cartProduct = await response.json();
+    setCartList(cartProduct.cart);
+  };
 
-  return <>
-  <div >{cartList.length>0 ? <div className="product-container">{cartList.map(cartItem =>{
-    const {
-      _id,
-      id,
-      url,
-      title,
-      rating,
-      price,
-      discount,
-      qty
-    } = cartItem;
-    let discountedPrice = (price - price * (discount / 100)).toFixed(2);
-     return <div className="card-container">
-      <div className="card-detail-container">
-      <div className="image-conatiner">
+  const onclickHandleQty = (id, type) => {
+    console.log(id, type);
+
+    incrementTheProduct(id, type);
+  };
+  const incrementTheProduct = async (id, type) => {
+    const response = await fetch(`/api/user/cart/${id}`, {
+      method: "Post",
+      headers: {
+        authorization: token,
+      },
+      body: JSON.stringify({ action: { type: type } }),
+    });
+    console.log("increment response", await response.json());
+  };
+
+  const totalPrice =
+    cartList.length > 0 &&
+    cartList.reduce((acc, curr) => {
+      acc = acc + curr.price * curr.qty;
+      return acc;
+    }, 0);
+
+  const handleToggleWishlist = (productItem) => {
+    let wishListProduct = { product: productItem };
+    if (wishlist.includes(productItem._id)) {
+      const updatedWishlist = wishlist.filter(
+        (item) => item !== productItem._id
+      );
+      setWishlist(updatedWishlist);
+      RemoveFromWishList(productItem._id);
+      toast.warn(`${productItem.title} removed from wishlist!`, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } else {
+      addWishList(wishListProduct);
+      setWishlist([...wishlist, productItem._id]);
+      toast.success(`${productItem.title} added to wishlist!`, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className="cart-page-container">
+        {cartList.length > 0 ? (
+          <div className="cart-product-container">
+            {cartList.map((cartItem) => {
+              const { _id, id, url, title, rating, price, discount, qty } =
+                cartItem;
+              let discountedPrice = (price - price * (discount / 100)).toFixed(
+                2
+              );
+              return (
+                <div className="card-container">
+                  <div className="card-detail-container">
+                    <div className="image-conatiner">
                       <img src={url} />
                       <div className="rating-container">
                         <span className="star">&#9733;</span>
@@ -126,33 +120,67 @@ const handleToggleWishlist = (productItem) => {
                       <p>{title}</p>
                       <div className="price-discount">
                         <div>
-                        <span><span>&#8377;</span>{discountedPrice}</span>
-                        <span className="discount" style={{ textDecoration: "line-through" }}>
-                          <span>&#8377;</span>
-                          {price}
-                        </span>
+                          <span>
+                            <span>&#8377;</span>
+                            {discountedPrice}
+                          </span>
+                          <span
+                            className="discount"
+                            style={{ textDecoration: "line-through" }}
+                          >
+                            <span>&#8377;</span>
+                            {price}
+                          </span>
                         </div>
-                        <div><span>{discount}% Off</span></div>
+                        <div>
+                          <span>{discount}% Off</span>
+                        </div>
                       </div>
                     </div>
 
-      <p>Quantity:<button onClick={()=>onclickHandleQty(_id,'decrement')} >-</button>{qty}<button onClick={()=>onclickHandleQty(_id,'increment')}>+</button></p>
-      </div>
-      <div className="wishList-container">
-      <button
-                  onClick={() => handleToggleWishlist(cartItem)}
-                  className={wishlist.includes(_id) ? 'wishlist-button-added' : 'wishlist-button-remove'}
-                >
-                </button>
+                    <p>
+                      Quantity:
+                      <button
+                        onClick={() => onclickHandleQty(_id, "decrement")}
+                      >
+                        -
+                      </button>
+                      {qty}
+                      <button
+                        onClick={() => onclickHandleQty(_id, "increment")}
+                      >
+                        +
+                      </button>
+                    </p>
+                  </div>
+                  <div className="wishList-container">
+                    <button
+                      onClick={() => handleToggleWishlist(cartItem)}
+                      className={
+                        wishlist.includes(_id)
+                          ? "wishlist-button-added"
+                          : "wishlist-button-remove"
+                      }
+                    ></button>
+                  </div>
+                  <button
+                    className="cart-btn"
+                    onClick={() => onClickRemoveFromCart(_id, title)}
+                  >
+                    Remove from Cart
+                  </button>
                 </div>
-      <button className="cart-btn" onClick={()=> onClickRemoveFromCart(_id,title)}>Remove from Cart</button>
-    </div>
-    })}
-    <div>
-      Total Price {totalPrice}
-    </div>
-    </div>:<p>Your Cart is Empty</p> }</div>
-
-    
-  </>
+              );
+            })}
+            
+          </div>
+        ) : (
+          <p>Your Cart is Empty</p>
+        )}
+        <div className="checkout-detail">
+            total
+        </div>
+      </div>
+    </>
+  );
 };
