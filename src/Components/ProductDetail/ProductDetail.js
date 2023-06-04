ad4589c92ac0754ccfe3ffd0c21e1dd288f99b97
Kirti Singh <kirtisingh1497@gmail.com>
2023-06-04 19:46:51 +0530

    feat: product detail page

--- src/Components/ProductDetail/ProductDetail.js
+++ src/Components/ProductDetail/ProductDetail.js
@@ -1,5 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { ProductListingcontext } from "../../Context/ProductListContext";
+import "./productDetail.css"
+import { Loader } from "../Loader/Loader";
 
 const ProductDetail = () => {
   const { storeProductId } = useContext(ProductListingcontext);
@@ -18,17 +20,32 @@ const ProductDetail = () => {
   useEffect(() => {
     getProductDetail();
   }, []);
+  console.log("store",storeProductDetail.title)
+ let discountedPrice = Math.ceil((storeProductDetail.price - storeProductDetail.price * (storeProductDetail.discount / 100)))
 
   return (
-    <div>
-      ProductDetail
-      {storeProductDetail && (
-        <div>
-          <p>{storeProductDetail.title}</p>
-          <p>{storeProductDetail.size}</p>
-          <p>{storeProductDetail.rating}</p>
-        </div>
-      )}
+    <div className="productDetail-container">
+      {(storeProductDetail.title !== undefined ) ? (
+       <div class="card">
+       <div class="card-image">
+         <img src={storeProductDetail.url} alt="Image" />
+       </div>
+       <div class="card-details">
+         <h2>{storeProductDetail.title}</h2>
+         <p>{storeProductDetail.description}</p> 
+         <div className="price-discount">
+                        <div>
+                        <span><span>&#8377;</span>{discountedPrice}</span>
+                        <span className="discount" style={{ textDecoration: "line-through" }}>
+                          <span>&#8377;</span>
+                          {storeProductDetail.price}
+                        </span>
+                        </div>
+                        <div><span>{storeProductDetail.discount}% Off</span></div>
+                      </div>
+       </div>
+     </div>
+      ) : <Loader/>}
     </div>
   );
 };
